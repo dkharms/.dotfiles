6c692b41cf7be16511ce1cb06f6f9726f64ae683
Daniil Porokhnin <dkharms@yandex-team.ru>
2023-02-16 11:16:42 +0300

    fix(dotfiles): fixed `init.sh` file
    Now symlinks creating in a correct way.

--- init.sh
+++ init.sh
@@ -1,32 +1,32 @@
-ROOT_PATH=$(pwd)
+INIT_PATH=$(pwd)
 
 # 🍺 brew configuration
-brew bundle
+# brew bundle
 
 # 🔥 zsh configuration
-sudo ln -sf ${ROOT_PATH}/.zshrc ${HOME}/.zshrc
+sudo ln -sf ${INIT_PATH}/.zshrc ${HOME}/.zshrc
 
 # 🌟 starship configuration
-sudo ln -sf ${ROOT_PATH}/config/starship/starship.toml \
+sudo ln -sf ${INIT_PATH}/config/starship/starship.toml \
   ${HOME}/.config/starship.toml
 
 # 🔥 alacritty configuration
 mkdir -p ${HOME}/.config/alacritty
-sudo ln -sf ${ROOT_PATH}/config/alacritty/alacritty.yml \
+sudo ln -sf ${INIT_PATH}/config/alacritty/alacritty.yml \
   ${HOME}/.config/alacritty/alacritty.yml
 
 # 🔥 helix configuration
 mkdir -p ${HOME}/.config/helix
-sudo ln -sf ${ROOT_PATH}/config/helix/config.toml ${HOME}/.config/helix/config.toml
-sudo ln -sf ${ROOT_PATH}/config/helix/languages.toml ${HOME}/.config/helix/languages.toml
+sudo ln -sf ${INIT_PATH}/config/helix/config.toml ${HOME}/.config/helix/config.toml
+sudo ln -sf ${INIT_PATH}/config/helix/languages.toml ${HOME}/.config/helix/languages.toml
 
-mkdir -p ${HOME}/.config/helix/themes && cd ${ROOT_PATH}/config/helix/themes
+mkdir -p ${HOME}/.config/helix/themes && cd ${INIT_PATH}/config/helix/themes
 for filename in ./*.toml; do
-  sudo ln -sf ${filename} \
+  sudo ln -sf $(pwd)/${filename} \
     ${HOME}/.config/helix/themes/${filename}
 done
-cd ${ROOT_PATH}
+cd ${INIT_PATH}
 
 # 🔥 git configuration
-sudo ln -sf ${ROOT_PATH}/.commitmessage ${HOME}/.commitmessage
-sudo ln -sf ${ROOT_PATH}/.gitconfig ${HOME}/.gitconfig
+sudo ln -sf ${INIT_PATH}/.commitmessage ${HOME}/.commitmessage
+sudo ln -sf ${INIT_PATH}/.gitconfig ${HOME}/.gitconfig
